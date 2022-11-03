83387fc65cfe2af2d5e0bc879dd16e8eaf9f90a7
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-03 23:16:38 +0530

    [Siddharth]Add: Flip Coin

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace BasicCoreProgram
+
+{
+
+    public class FlipCoin
+    {
+        Random random = new Random();
+        public void Play()
+        {
+            double head = 0, tail = 0;
+            Console.WriteLine("No. of times you want to flip the coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double ACTUAL_COUNT = num;
+            while (num > 0)
+            {
+                if (random.NextDouble() > 0.5)
+                    head++;
+                else
+                    tail++;
+                num--;
+            }
+            Console.WriteLine("Head will come - " + head);
+            Console.WriteLine("Tail will come - " + tail);
+            double HEAD_PERCENTAGE = (head / ACTUAL_COUNT) * 100;
+            double TAIL_PERCENTAGE = (tail / ACTUAL_COUNT) * 100;
+            Console.WriteLine("Head Percenage will be - " + HEAD_PERCENTAGE + "\n" + "Tail Percentage will be - " + TAIL_PERCENTAGE);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BasicCoreProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Exit");
+                Console.WriteLine("Enter The Above Mention Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin enter = new FlipCoin();
+                        enter.Play();
+                        break;
+                    case 2:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
+
